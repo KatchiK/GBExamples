e80cea419ce6d297119b1c3451c2e1289aa6760a
KatchiK <a_kachalov@mail.ru>
2023-02-09 15:18:13 +0300

    Добавлена программа для нахождения самого большого числа

---
+++ Example006_IronMan/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 34;
+int b = 49;
+int c = 23;
+int e = 15;
+int d = 9;
+int f = 15;
+
+int max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+if (e>max) max = e;
+if (d>max) max = d;
+if (f>max) max = f;
+
+Console.Write("Самое большое число: ");
+Console.WriteLine(max);
