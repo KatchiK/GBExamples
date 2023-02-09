72226fae129415ecc061c000630eb0ca9218337a
KatchiK <a_kachalov@mail.ru>
2023-02-09 14:38:23 +0300

    Добавлена программа с Машей

---
+++ Example005_If/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Пожалуйста, введите имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+  Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}
