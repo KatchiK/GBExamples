8fec04b74225775eaf15c0ecbb652f8323f08cf6
Aleksey Kachalov <a_kachalov@mail.ru>
2023-02-12 18:36:47 +0300

    Добавлена задача максимальное из 9 чисел с помощью функций(методов) и массива

---
+++ Example010_introArray/Program.cs
@@ -0,0 +1,15 @@
+﻿//Находим максимальное из 9 чисел с помощью функций(методов) и массива
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array={ 10,1923,14,6,1234,745,24,21262,1345};
+
+int maxMax = Max(Max(array[0],array[1],array[2]),Max(array[3],array[4],array[5]),Max(array[6],array[7],array[8]));
+Console.WriteLine(maxMax);
+
