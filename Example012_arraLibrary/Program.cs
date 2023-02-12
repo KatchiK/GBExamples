dab61c9bf9fbeff008a13f0433f358f6dfa3357a
Aleksey Kachalov <a_kachalov@mail.ru>
2023-02-12 19:36:21 +0300

    Добавлены задачи с массивами

--- Example012_arraLibrary/Program.cs
+++ Example012_arraLibrary/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int[] array = new int[10];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index]=new Random().Next(1,11);
+        index++;
+    }
+}
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = -1;
+    while (position < count)
+    {
+        Console.WriteLine(coll[position]);
+        position++;
+    }
+}
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+Console.Clear();
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOff(array, 4);
+Console.WriteLine(pos);
+
