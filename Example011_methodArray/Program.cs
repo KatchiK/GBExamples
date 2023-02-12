985fd2437c4048c4a628d265845b0840c53bee57
Aleksey Kachalov <a_kachalov@mail.ru>
2023-02-12 18:56:34 +0300

    Добавлена задача по нахождению индекса нужного элемента в массиве

--- Example011_methodArray/Program.cs
+++ Example011_methodArray/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+int[] array={ 10,1923,14,6,1234,745,24,21262,1345};
+
+int n = array.Length;
+
+int find = 746;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    
+    index++;
+}
