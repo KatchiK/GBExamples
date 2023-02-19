32ffb147e4bae901a0717c8fb8d3c43c1fcd4799
Aleksey Kachalov <a_kachalov@mail.ru>
2023-02-19 19:53:21 +0300

    добавлены задачи к 3-й лекции

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// 1-й тип функций
+void Method1();
+{
+    Console.WriteLine(Athor: Alex);
+}
+Method1();//вызов метода (функции)
+
+// 2-й тип функций
+void Method2(string msg);
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текст сообщения");
+
+// 3-й тип функций
+int Method3()
+{
+    return DataTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+//4-й тип функций
+string Method4(int count, string text)
+{
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "Раз");
+Console.WriteLine();
---
+++ Lesson3/Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Сортировка массива. (Метод максимального)
+int[] arr = { 3, 5, 2, 6, 1, 13, 62, 1634, 14, 34, 9, 6, 13 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
